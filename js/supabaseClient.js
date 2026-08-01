96942d95fcf453ee2e2421c8709c626a75090c2a
Ayush <ayushhsingh1811@gmail.com>
2026-08-01 22:25:58 +0530

    M

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,8 +2,8 @@
  * Supabase Configuration and Initialization
  */
 
-const SUPABASE_URL = 'https://epncydxcvuketsdnxnhz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwbmN5ZHhjdnVrZXRzZG54bmh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1MDQ3OTQsImV4cCI6MjA5OTA4MDc5NH0.nHBp53HfK0gs8zqoAmBooStd5KtffM6OI1UAfuHIQTA';
+const SUPABASE_URL = 'https://romdfgbohhmwsjtzphzj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvbWRmZ2JvaGhtc3dqdHpwaHpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1OTgyNTQsImV4cCI6MjEwMTE3NDI1NH0.fmOFNjHU55zXO-Fpy8X0bhaVkANOUb8KEcr3HxjrDxo';
 
 // Initialize the Supabase client
 // This expects the global 'supabase' object from the CDN script
